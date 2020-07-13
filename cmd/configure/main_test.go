4a5fcc314a386353f0527438285d7e586cb701d1
Ryan Moran <rmoran@pivotal.io>
2020-07-13 10:45:34 -0400

    Removes libcfbuildpack from configure command

--- cmd/configure/main_test.go
+++ cmd/configure/main_test.go
@@ -1,22 +1,7 @@
-/*
- * Copyright 2018-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package main
+package main_test
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -24,90 +9,196 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/cloudfoundry/libcfbuildpack/test"
-	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
 	"github.com/sclevine/spec"
 	"github.com/sclevine/spec/report"
-)
 
-var (
-	pathToCli string
+	. "github.com/onsi/gomega"
 )
 
 func TestUnitConfigure(t *testing.T) {
-	spec.Run(t, "Configure", testConfigure, spec.Report(report.Terminal{}))
-}
-
-func runCli(tmpDir, body string, env []string, localModulePath, globalModulePath string) string {
-	Expect(ioutil.WriteFile(filepath.Join(tmpDir, "nginx.conf"), []byte(body), 0644)).To(Succeed())
-
-	command := exec.Command(pathToCli, filepath.Join(tmpDir, "nginx.conf"), localModulePath, globalModulePath)
-	command.Env = env
-	session, err := gexec.Start(command, os.Stdout, os.Stderr)
-	Expect(err).ToNot(HaveOccurred())
-	Eventually(session).Should(gexec.Exit(0))
-
-	output, err := ioutil.ReadFile(filepath.Join(tmpDir, "nginx.conf"))
-	Expect(err).ToNot(HaveOccurred())
-
-	return string(output)
-}
-
-func testConfigure(t *testing.T, when spec.G, it spec.S) {
-	var factory *test.DetectFactory
 	var (
-		localModulePath, globalModulePath string
+		Expect     = NewWithT(t).Expect
+		Eventually = NewWithT(t).Eventually
 	)
 
-	it.Before(func() {
-		var err error
+	path, err := gexec.Build("github.com/paketo-buildpacks/nginx/cmd/configure")
+	Expect(err).ToNot(HaveOccurred())
 
-		RegisterTestingT(t)
-		factory = test.NewDetectFactory(t)
-		pathToCli, err = gexec.Build("github.com/paketo-buildpacks/nginx/cmd/configure")
-		Expect(err).ToNot(HaveOccurred())
-	})
+	defer gexec.CleanupBuildArtifacts()
 
-	when("it runs", func() {
+	spec.Run(t, "Configure", func(t *testing.T, context spec.G, it spec.S) {
+		var (
+			localModulePath  string
+			globalModulePath string
+			workingDir       string
+
+			command *exec.Cmd
+			buffer  *bytes.Buffer
+		)
+
 		it.Before(func() {
-			test.WriteFile(t, filepath.Join(factory.Home, "nginx.conf"), "")
+			var err error
+			workingDir, err = ioutil.TempDir("", "working-dir")
+			Expect(err).NotTo(HaveOccurred())
 		})
 
-		it("templates current port using the 'port' func", func() {
-			body := runCli(factory.Home, "Hi the port is {{port}}.", []string{"PORT=8080"}, "", "")
-			Expect(body).To(Equal("Hi the port is 8080."))
+		it.After(func() {
+			Expect(os.RemoveAll(workingDir)).To(Succeed())
 		})
 
-		it("templates environment variables using the 'env' func", func() {
-			body := runCli(factory.Home, `The env var FOO is {{env "FOO"}}`, []string{"FOO=BAR"}, "", "")
-			Expect(body).To(Equal("The env var FOO is BAR"))
-		})
-
-		when("templating a load_module directive using the 'module' func", func() {
+		context("when the template contains a 'port' action", func() {
 			it.Before(func() {
-				localModulePath = filepath.Join(factory.Home, "local_modules")
-				globalModulePath = filepath.Join(factory.Home, "global_modules")
+				Expect(ioutil.WriteFile(filepath.Join(workingDir, "nginx.conf"), []byte("Hi the port is {{port}}."), 0644)).To(Succeed())
 
+				command = exec.Command(path, filepath.Join(workingDir, "nginx.conf"), localModulePath, globalModulePath)
+				command.Env = []string{"PORT=8080"}
+
+				buffer = bytes.NewBuffer(nil)
+			})
+
+			it("inserts the port value into that location in the text", func() {
+				session, err := gexec.Start(command, buffer, buffer)
+				Expect(err).ToNot(HaveOccurred())
+
+				Eventually(session).Should(gexec.Exit(0), buffer.String)
+
+				output, err := ioutil.ReadFile(filepath.Join(workingDir, "nginx.conf"))
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(string(output)).To(Equal("Hi the port is 8080."))
+			})
+		})
+
+		context("when the template contains an 'env' action", func() {
+			it.Before(func() {
+				Expect(ioutil.WriteFile(filepath.Join(workingDir, "nginx.conf"), []byte(`The env var FOO is {{env "FOO"}}`), 0644)).To(Succeed())
+
+				command = exec.Command(path, filepath.Join(workingDir, "nginx.conf"), localModulePath, globalModulePath)
+				command.Env = []string{"FOO=BAR"}
+
+				buffer = bytes.NewBuffer(nil)
+			})
+
+			it("inserts the env variable into that location in the text", func() {
+				session, err := gexec.Start(command, buffer, buffer)
+				Expect(err).ToNot(HaveOccurred())
+
+				Eventually(session).Should(gexec.Exit(0), buffer.String)
+
+				output, err := ioutil.ReadFile(filepath.Join(workingDir, "nginx.conf"))
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(string(output)).To(Equal("The env var FOO is BAR"))
+			})
+		})
+
+		context("templating a load_module directive using the 'module' func", func() {
+			it.Before(func() {
+				localModulePath = filepath.Join(workingDir, "local_modules")
 				Expect(os.Mkdir(localModulePath, 0744)).To(Succeed())
-				Expect(os.Mkdir(globalModulePath, 0744)).To(Succeed())
 				Expect(ioutil.WriteFile(filepath.Join(localModulePath, "local.so"), []byte("dummy data"), 0644)).To(Succeed())
+
+				globalModulePath = filepath.Join(workingDir, "global_modules")
+				Expect(os.Mkdir(globalModulePath, 0744)).To(Succeed())
 				Expect(ioutil.WriteFile(filepath.Join(globalModulePath, "global.so"), []byte("dummy data"), 0644)).To(Succeed())
 			})
 
-			when("when the module is in local modules directory", func() {
+			context("when the module is in local modules directory", func() {
+				it.Before(func() {
+					Expect(ioutil.WriteFile(filepath.Join(workingDir, "nginx.conf"), []byte(`{{module "local"}}`), 0644)).To(Succeed())
+
+					command = exec.Command(path, filepath.Join(workingDir, "nginx.conf"), localModulePath, globalModulePath)
+					buffer = bytes.NewBuffer(nil)
+				})
+
 				it("loads the module from the local directory", func() {
-					body := runCli(factory.Home, `{{module "local"}}`, nil, localModulePath, globalModulePath)
-					Expect(body).To(Equal(fmt.Sprintf("load_module %s/local.so;", localModulePath)))
+					session, err := gexec.Start(command, buffer, buffer)
+					Expect(err).ToNot(HaveOccurred())
+
+					Eventually(session).Should(gexec.Exit(0), buffer.String)
+
+					output, err := ioutil.ReadFile(filepath.Join(workingDir, "nginx.conf"))
+					Expect(err).ToNot(HaveOccurred())
+
+					Expect(string(output)).To(Equal(fmt.Sprintf("load_module %s/local.so;", localModulePath)))
 				})
 			})
 
-			when("when the module is in global modules directory", func() {
+			context("when the module is in global modules directory", func() {
+				it.Before(func() {
+					Expect(ioutil.WriteFile(filepath.Join(workingDir, "nginx.conf"), []byte(`{{module "global"}}`), 0644)).To(Succeed())
+
+					command = exec.Command(path, filepath.Join(workingDir, "nginx.conf"), localModulePath, globalModulePath)
+					buffer = bytes.NewBuffer(nil)
+				})
+
 				it("loads the module from the global directory", func() {
-					body := runCli(factory.Home, `{{module "global"}}`, nil, localModulePath, globalModulePath)
-					Expect(body).To(Equal(fmt.Sprintf("load_module %s/global.so;", globalModulePath)))
+					session, err := gexec.Start(command, buffer, buffer)
+					Expect(err).ToNot(HaveOccurred())
+
+					Eventually(session).Should(gexec.Exit(0), buffer.String)
+
+					output, err := ioutil.ReadFile(filepath.Join(workingDir, "nginx.conf"))
+					Expect(err).ToNot(HaveOccurred())
+
+					Expect(string(output)).To(Equal(fmt.Sprintf("load_module %s/global.so;", globalModulePath)))
 				})
 			})
 		})
-	})
+
+		context("failure cases", func() {
+			context("when the template file does not exist", func() {
+				it.Before(func() {
+					command = exec.Command(path, "/no/such/template.conf", localModulePath, globalModulePath)
+					buffer = bytes.NewBuffer(nil)
+				})
+
+				it("prints an error and exits non-zero", func() {
+					session, err := gexec.Start(command, buffer, buffer)
+					Expect(err).ToNot(HaveOccurred())
+
+					Eventually(session).Should(gexec.Exit(1), buffer.String)
+
+					Expect(buffer.String()).To(Equal("failed to parse template: open /no/such/template.conf: no such file or directory\n"))
+				})
+			})
+
+			context("when the template file cannot be written", func() {
+				it.Before(func() {
+					Expect(ioutil.WriteFile(filepath.Join(workingDir, "nginx.conf"), []byte(`{{module "global"}}`), 0444)).To(Succeed())
+
+					command = exec.Command(path, filepath.Join(workingDir, "nginx.conf"), localModulePath, globalModulePath)
+					buffer = bytes.NewBuffer(nil)
+				})
+
+				it("prints an error and exits non-zero", func() {
+					session, err := gexec.Start(command, buffer, buffer)
+					Expect(err).ToNot(HaveOccurred())
+
+					Eventually(session).Should(gexec.Exit(1), buffer.String)
+
+					Expect(buffer.String()).To(MatchRegexp("failed to create nginx.conf: .*: permission denied\n"))
+				})
+			})
+
+			context("when the template is malformed", func() {
+				it.Before(func() {
+					Expect(ioutil.WriteFile(filepath.Join(workingDir, "nginx.conf"), []byte(`{{ port "argument" }}`), 0644)).To(Succeed())
+
+					command = exec.Command(path, filepath.Join(workingDir, "nginx.conf"), localModulePath, globalModulePath)
+					buffer = bytes.NewBuffer(nil)
+				})
+
+				it("prints an error and exits non-zero", func() {
+					session, err := gexec.Start(command, buffer, buffer)
+					Expect(err).ToNot(HaveOccurred())
+
+					Eventually(session).Should(gexec.Exit(1), buffer.String)
+
+					Expect(buffer.String()).To(MatchRegexp("failed to execute template: .*: wrong number of args for port: want 0 got 1\n"))
+				})
+			})
+		})
+	}, spec.Report(report.Terminal{}))
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,7 @@ module github.com/paketo-buildpacks/nginx
 go 1.12
 
 require (
-	cloud.google.com/go v0.55.0 // indirect
 	github.com/BurntSushi/toml v0.3.1
-	github.com/cloudfoundry/libcfbuildpack v1.91.23
 	github.com/onsi/gomega v1.10.1
 	github.com/paketo-buildpacks/occam v0.0.16
 	github.com/paketo-buildpacks/packit v0.2.0
