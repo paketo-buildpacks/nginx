7646f725305db9a37118a5245de0586434111142
TisVictress <vhenry@vmware.com>
2022-04-20 15:17:16 -0400

    Allow users to set custom nginx.conf location (#373)

---
+++ integration/custom_conf_app_test.go
@@ -0,0 +1,79 @@
+package integration_test
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/paketo-buildpacks/occam"
+
+	"github.com/sclevine/spec"
+
+	. "github.com/onsi/gomega"
+	. "github.com/paketo-buildpacks/occam/matchers"
+)
+
+func testCustomConfApp(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect     = NewWithT(t).Expect
+		Eventually = NewWithT(t).Eventually
+
+		pack   occam.Pack
+		docker occam.Docker
+
+		name      string
+		source    string
+		image     occam.Image
+		container occam.Container
+	)
+
+	it.Before(func() {
+		pack = occam.NewPack().WithNoColor().WithVerbose()
+		docker = occam.NewDocker()
+
+		var err error
+		name, err = occam.RandomName()
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	it.After(func() {
+		Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
+		Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
+		Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
+		Expect(os.RemoveAll(source)).To(Succeed())
+	})
+
+	context("when pushing an app with a custom nginx.conf location", func() {
+		it.Before(func() {
+			var err error
+			source, err = occam.Source(filepath.Join("testdata", "custom_conf_app"))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it("serves up staticfile", func() {
+			var err error
+			image, _, err = pack.Build.
+				WithBuildpacks(nginxBuildpack).
+				WithPullPolicy("never").
+				WithEnv(map[string]string{
+					"BP_NGINX_CONF_LOCATION": "conf/nginx.conf",
+				}).
+				Execute(name, source)
+			Expect(err).NotTo(HaveOccurred())
+
+			container, err = docker.Container.Run.
+				WithEnv(map[string]string{"PORT": "8080"}).
+				WithPublish("8080").
+				Execute(image.ID)
+			Expect(err).ToNot(HaveOccurred())
+
+			Eventually(container).Should(BeAvailable())
+
+			response, err := http.Get(fmt.Sprintf("http://localhost:%s/index.html", container.HostPort("8080")))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(response.StatusCode).To(Equal(http.StatusOK))
+		})
+	})
+}
