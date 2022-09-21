56a0c628353e60ee9de9d4633ea1fb911986a3f0
Joshua Casey <joshuatcasey@gmail.com>
2022-09-20 19:38:04 -0500

    Use t.Setenv instead of os.Setenv

--- cmd/configure/internal/run_test.go
+++ cmd/configure/internal/run_test.go
@@ -44,7 +44,7 @@ func testRun(t *testing.T, context spec.G, it spec.S) {
 	context("when the template contains a 'port' action", func() {
 		it.Before(func() {
 			Expect(os.WriteFile(mainConf, []byte("Hi the port is {{port}}."), 0600)).To(Succeed())
-			os.Setenv("PORT", "8080")
+			t.Setenv("PORT", "8080")
 		})
 
 		it("inserts the port value into that location in the text", func() {
@@ -77,7 +77,7 @@ func testRun(t *testing.T, context spec.G, it spec.S) {
 	context("when the template contains an 'env' action", func() {
 		it.Before(func() {
 			Expect(os.WriteFile(filepath.Join(workingDir, "nginx.conf"), []byte(`The env var FOO is {{env "FOO"}}`), 0600)).To(Succeed())
-			os.Setenv("FOO", "BAR")
+			t.Setenv("FOO", "BAR")
 		})
 
 		it("inserts the env variable into that location in the text", func() {
@@ -148,7 +148,7 @@ func testRun(t *testing.T, context spec.G, it spec.S) {
 	port_in_redirect off; # Ensure that redirects don't include the internal container PORT - 8080
 	}`), 0600)).To(Succeed())
 				Expect(os.WriteFile(filepath.Join(workingDir, "custom.conf"), []byte(`Hi the port is {{ port }}.`), 0600)).To(Succeed())
-				os.Setenv("PORT", "8080")
+				t.Setenv("PORT", "8080")
 			})
 
 			it("parses 'include' file and interpolates values", func() {
@@ -177,7 +177,7 @@ func testRun(t *testing.T, context spec.G, it spec.S) {
 				Expect(os.WriteFile(filepath.Join(workingDir, "dontFix.conf"), []byte(`Hi the port is {{ port }}.`), 0600)).To(Succeed())
 				Expect(os.WriteFile(filepath.Join(workingDir, "subdir", "custom1.conf"), []byte(`Hi the port is {{ port }}.`), 0600)).To(Succeed())
 				Expect(os.WriteFile(filepath.Join(workingDir, "subdir", "custom2.conf"), []byte(`Hi the port is {{ port }}.`), 0600)).To(Succeed())
-				os.Setenv("PORT", "8080")
+				t.Setenv("PORT", "8080")
 			})
 
 			it("parses 'include' files and interpolates values into all files that match the mask", func() {
